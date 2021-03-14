a97b8d8651e23ff3fb98f08fca61a0276959a9b6
laclaverie.pierre <pierre.laclaverie@telecom-st-etienne.fr>
2021-03-14 03:35:53 +0100

    avec save .mat

--- ProjetTextureFonctionalise.m
+++ ProjetTextureFonctionalise.m
@@ -5,9 +5,9 @@ warning('off','all'); % Supprimer les warnings
 close all;
 clear all;
 clc
-nb_images_ref = 10;
-quantification= 64;
-nb_test=5;
+nb_images_ref = 25;
+quantification= 100;
+nb_test=15;
 veriteTerrain =[];
 sortie_algo=[];
 %% Initialiser les categories
@@ -17,10 +17,16 @@ creer_dossier_base_ref(rep,fin,nb_images_ref,quantification);
 %% Creer la signature de chaque image de référence
 rep_ref = 'baseRef/';
 tic;
-[sig,nom,tab] = signatures_images_ref(rep_ref,'histogrammeLBP',quantification,fin,nb_images_ref);
+if exist ('data') ==0 % Si baseref existe pas, on la créé
+    disp('je suis là');
+    [sig,nom,tab] = signatures_images_ref(rep_ref,'histogrammeLBP',quantification,fin,nb_images_ref);
+    save('data','sig','nom','tab');
+else
+    load('data');
+end
 toc;
-%% choisir une image test aleatoirement dans la banque d'images
 
+%% choisir une image test aleatoirement dans la banque d'images
 for test=1 :nb_test
    
 [img_ref,categorie_ref,rd] = choisir_image_aleatoirement(rep,nb_images_ref); % Choisir une image dans le repertoire en enlevant les images de reference
@@ -31,21 +37,20 @@ signature_ref= histogrammeLBP(img_ref);
 %% Afficher les resultats
 % affiche des 5 plus proches, distance décroissante 
  matches =0;
- figure;
-subplot(2,3,1); imshow(img_ref);title(sprintf('%s : n° dossier : %d \n Categorie : %s ',rep{rd},rd,categorie_ref));
+ %figure;
+%subplot(2,3,1); imshow(img_ref);title(sprintf('%s : n° dossier : %d \n Categorie : %s ',rep{rd},rd,categorie_ref));
 for sim=1:5 
     if nom{3,Trie(sim,2)}==categorie_ref
         matches=matches+1;
     end
-    legende=sprintf('%s \n %.5f \n Categorie : %s',nom{1,Trie(sim,2)},Trie(sim,1),nom{3,Trie(sim,2)});
-    subplot(2,3,sim+1); imshow(tab{Trie(sim,2)}); title(legende);
+    %legende=sprintf('%s \n %.5f \n Categorie : %s',nom{1,Trie(sim,2)},Trie(sim,1),nom{3,Trie(sim,2)});
+    %subplot(2,3,sim+1); imshow(tab{Trie(sim,2)}); title(legende);
 end  
 fprintf('Bonnes categories : %d \n ',matches);
 
 %% Matrice de confusion
 veriteTerrain= [veriteTerrain categorie_ref];
-sortie_algo = [sortie_algo nom{3,Trie(1,2)}];
-
+sortie_algo = [sortie_algo nom{3,Trie(1,2)}];% Texture la plus proche
 end
 % Affichage
 c= confusionmat(veriteTerrain,sortie_algo)
